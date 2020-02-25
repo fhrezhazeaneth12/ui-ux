e501d1ea73b4db4fc8ed1905b2e417d65a7c4eb3
Reo Sahobby <reoshby@gmail.com>
2020-02-25 07:42:32 +0700

    update chart dan menu link

--- chart.php
+++ chart.php
@@ -5,7 +5,7 @@
  <!doctype html>
 <html lang="en">
   <head>
-  	<title>Aplikasi Ui/Ux</title>
+  	<title>Chart Ui/Ux</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -29,13 +29,13 @@
 	  			</div>
 	  		</div>
         <ul class="list-unstyled components mb-5">
-          <li class="active">
+          <li>
             <a href="home.php"><span class="fa fa-home mr-3"></span> Home</a>
           </li>
           <li>
               <a href="#"><span class="fa fa-download mr-3 notif"><small class="d-flex align-items-center justify-content-center">5</small></span> Download</a>
           </li>
-          <li>
+          <li class="active">
             <a href="chart.php"><span class="fa fa-gift mr-3"></span> Chart</a>
           </li>
           <li>
@@ -56,7 +56,7 @@
 
         <!-- Page Content  -->
       <div id="content" class="p-4 p-md-5 pt-5">
-        <h2 class="mb-4">Haloooo</h2>
+        <h2 class="mb-4">Chart</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
